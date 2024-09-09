e8ba2a6b2db31f075a2ee16bda34a4473e2599e0
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2024-09-09 17:06:57 +0200

    Changed the graphWriter function to receive the path to write to

--- utils/graph/graph.h
+++ utils/graph/graph.h
@@ -46,8 +46,7 @@ struct GraphStruct {
 	}
 
 	// Function that saves to file the contents of the current graph
-	void graphWriter() {
-		char path[] = "/content/testing/graph.txt";
+	void graphWriter(char *path) {
 		FILE *fptr;
 
 		// Open the file in writing mode
@@ -62,7 +61,7 @@ struct GraphStruct {
 		// Write the adjacency lists
 		for(node i = 0; i < nodeSize; ++i) {
 			for (node j = 0; j < deg(i); j++) {
-				fprintf(fptr, "%d,", neighs[cumDegs[i] + j]);
+				fprintf(fptr, "%d,%f", neighs[cumDegs[i] + j], weights[cumDegs[i] + j]);
 			}
 			fprintf(fptr, "\n");
 		}
